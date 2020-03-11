8586b1e77b0a9e8b98fb94bdf532ca462e7aa8f6
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-03-11 15:36:16 +0900

    コントローラにNewメソッドを追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,4 +6,11 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new 
   end
+
+  def create
+    #paramsメソッドを使用することで、引数を取得できる。
+    #送られてきた引数は{"blog"=>{"title"=>"hoge", "content"=>"fuga"}}配列の中に配列が入っているため、下記のように取り出している。
+    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+  end
+
 end
