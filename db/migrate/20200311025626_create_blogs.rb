c81bf28674f134cf088820ac3c76001859a8d6c8
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-03-11 13:30:11 +0900

    マイグレートファイルにテーブル定義を追加

--- db/migrate/20200311025626_create_blogs.rb
+++ db/migrate/20200311025626_create_blogs.rb
@@ -1,6 +1,12 @@
 class CreateBlogs < ActiveRecord::Migration[5.2]
+#新しくマイグレーションファイルを作成したときは以下のコマンドを実行することで、DBに反映できる。
+#rails db:migrate
   def change
     create_table :blogs do |t|
+    #t.型：列名
+    t.string :title
+    t.text :content
+    t.timestamps
     end
   end
 end
