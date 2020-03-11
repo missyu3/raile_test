9fdef9612847e6f5c0a2a4874e7b8f0b1401358a
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-03-11 17:22:42 +0900

    Strong Parametersを指定

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,7 +10,13 @@ class BlogsController < ApplicationController
   def create
     #paramsメソッドを使用することで、引数を取得できる。
     #送られてきた引数は{"blog"=>{"title"=>"hoge", "content"=>"fuga"}}配列の中に配列が入っているため、下記のように取り出している。
-    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+    #Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+    
+    #Strong Parametersで渡す。
+    #Strong Parametersは送られてきたpermitの何を許可するかを指定します。
+    #permitで指定されていない列は渡されないようになっている。
+    Blog.create(params.require(:blog).permit(:title,:content))
+
 
     #リダイレクト：指定したWebページから自動的に別のWebページに転送されること
     #リダイレクトを行うときはURLではなく、Prefixで指定するのが一般的。
